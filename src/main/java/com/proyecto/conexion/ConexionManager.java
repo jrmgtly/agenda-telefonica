330d6f7f0ce50a74945cc067eb3b29819cb9fc72
jrmgtly <156686282+jrmgtly@users.noreply.github.com>
2025-05-30 06:30:24 -0700

    Conexion a base de datos y archivo Pom.xml

---
+++ src/main/java/com/proyecto/conexion/ConexionManager.java
@@ -0,0 +1,18 @@
+package com.proyecto.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionManager {
+
+	private static final String URL = "jdbc:mariadb://localhost:3306/bd_agenda_telefonica";
+	private static final String USER = "root";
+	private static final String PASSWORD = "3252";
+	
+	public static Connection obtenerConexion() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+		
+	}
+	
+}
